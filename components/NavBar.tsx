1c630ff53f26020e7e3987f2b872f12e70e4616b
Jan Dalhuysen <dalhuysen.j@gmail.com>
2022-01-03 15:23:00 +0200

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,15 +2,16 @@ export default function NavBar({ handleClick }: { handleClick: any }) {
   return (
     <div className="rounded-md w-full fixed z-10 left-0 top-3 bg-black bg-opacity-90 flex items-center text-white font-bold px-4 py-3 md:py-4 justify-between shadow-md">
       <p className="sm:text-xl text-lg sm:pr-4 pr-2 border-r cursor-pointer border-gray-500">
-        Miner&apos;s <span className="text-blue-400">nation</span>
+        Miner&apos;s <span className="text-blue-400">Nation</span>
       </p>
       <div className=" items-center pl-4 hidden md:flex">
         <NavLinks text="Home" />
         <NavLinks text="About" />
-        <NavLinks text="How it works" />
-        <NavLinks text="Press" />
-        <NavLinks text="Request demo" />
-        <NavLinks text="Shop" />
+        <NavLinks text="Roadmap" />
+        <NavLinks text="Tokenomics" />
+        <NavLinks text="Team" />
+        <NavLinks text="Join Pre-Sale" />
+        <NavLinks text="Claim Airdrop" />
       </div>
       <span
         className="block md:hidden p-1 sm:p-2 rounded-full bg-white ease-in ml-4 cursor-pointer"
