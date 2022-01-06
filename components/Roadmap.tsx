cf7442918022abcc66aae7f406728435b1caf9a8
Jan Dalhuysen <dalhuysen.j@gmail.com>
2022-01-06 15:13:33 +0200

    Update Roadmap.tsx

--- components/Roadmap.tsx
+++ components/Roadmap.tsx
@@ -1,4 +1,10 @@
 import styles from "../styles/nav.module.css";
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
 
 export default function Roadmap() {
   return (
