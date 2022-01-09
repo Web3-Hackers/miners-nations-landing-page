2d7fb4c4044f83910bf0ec97d925cd36d686b4db
Jan Dalhuysen <dalhuysen.j@gmail.com>
2022-01-09 15:18:39 +0200

    Update Tokenomics.tsx

--- components/Tokenomics.tsx
+++ components/Tokenomics.tsx
@@ -1,4 +1,23 @@
 import styles from "../styles/nav.module.css";
+import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer } from 'recharts';
+
+const data01 = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+  { name: 'Group C', value: 300 },
+  { name: 'Group D', value: 200 },
+  { name: 'Group E', value: 278 },
+  { name: 'Group F', value: 189 },
+];
+
+const data02 = [
+  { name: 'Group A', value: 2400 },
+  { name: 'Group B', value: 4567 },
+  { name: 'Group C', value: 1398 },
+  { name: 'Group D', value: 9800 },
+  { name: 'Group E', value: 3908 },
+  { name: 'Group F', value: 4800 },
+];
 
 export default function Tokenomics() {
   return (
@@ -10,6 +29,20 @@ export default function Tokenomics() {
           ipsum lorem loro lorem ipsum doret lorum lorem ipsum lorem doro lorum
           lorem ipsum lorem doro.
         </p>
+        <PieChart width={400} height={400}>
+          <Pie
+            dataKey="value"
+            isAnimationActive={false}
+            data={data01}
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
+            fill="#8884d8"
+            label
+          />
+          <Pie dataKey="value" data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" />
+          <Tooltip />
+        </PieChart>
         <button
           className={`text-center p-3 bg-blue-500 font-bold rounded-md ${styles.btn}`}
         >
