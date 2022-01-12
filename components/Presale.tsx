6a7f8941a666f850f29debbd6859f6af02a26482
Jan Dalhuysen <dalhuysen.j@gmail.com>
2022-01-12 11:39:30 +0200

    Update Presale.tsx

--- components/Presale.tsx
+++ components/Presale.tsx
@@ -4,7 +4,7 @@ export default function Presale() {
   return (
     <div className="flex items-center flex-wrap md:flex-nowrap bg-blue-200 md:px-24 py-24 px-4 ">
       <div className="md:w-1/2 w-full">
-        <p className="font-semibold text-4xl sm:text-4xl pb-4">Join Pre-Sale Text</p>
+        <p className="font-semibold text-4xl sm:text-4xl pb-4">Join Pre-Sale</p>
         <p className="font-light sm:text-lg pb-4">
           Loren ipsum ipsum lorem ipsum doret loro lorem ipsum doret loro lorem
           ipsum lorem loro lorem ipsum doret lorum lorem ipsum lorem doro lorum
@@ -16,7 +16,6 @@ export default function Presale() {
           Join Pre-Sale
         </button>
       </div>
-      <div className="md:w-1/2 mt-6 md:mt-0 w-full h-48 md:ml-4 ring-1 ring-blue-300"></div>
     </div>
   );
 }
