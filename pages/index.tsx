a384f925ec0ed3d0fcc0d6d62b0c6cff884e93a2
Jan Dalhuysen <dalhuysen.j@gmail.com>
2022-01-12 11:06:05 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,6 @@ const Home: NextPage = () => {
         {click && <Overlay handleClick={handleClick} />}
         <Header handleClick={handleClick} />
         <HomeSect />
-        <ClaimDrop />
         <About />
         <Roadmap />
         <Tokenomics />
